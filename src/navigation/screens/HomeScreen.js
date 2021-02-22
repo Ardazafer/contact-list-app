a8ff9b39fd22290d516df2c516515cab922e8247
Arda Zafer Ibin <z.ardaibin@gmail.com>
2021-02-22 05:37:06 +0300

    Created the contact button

--- src/navigation/screens/HomeScreen.js
+++ src/navigation/screens/HomeScreen.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Button} from 'react-native';
 
 export default function HomeScreen() {
   return (
     <View>
-      <Text>Home Screen</Text>
+      <Button title="Open Contact List" onPress={() => {}}></Button>
     </View>
   );
 }
